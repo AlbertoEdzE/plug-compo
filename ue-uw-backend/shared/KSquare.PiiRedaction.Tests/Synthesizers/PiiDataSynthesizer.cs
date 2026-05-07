876c6e2b229d747ba0a8d8ce66a2c3e012430790
Alberto Hernandez <albertohernandezespinosa@gmail.com>
2026-05-07 16:23:56 -0600

    test(kspl-006): add data synthesizer

---
+++ ue-uw-backend/shared/KSquare.PiiRedaction.Tests/Synthesizers/PiiDataSynthesizer.cs
@@ -0,0 +1,24 @@
+using Bogus;
+
+namespace KSquare.PiiRedaction.Tests.Synthesizers;
+
+public sealed class PiiDataSynthesizer
+{
+    private readonly Faker _faker;
+
+    public PiiDataSynthesizer(int seed = 1337)
+    {
+        Randomizer.Seed = new Random(seed);
+        _faker = new Faker();
+    }
+
+    public string Email() => _faker.Internet.Email();
+
+    public string Phone() => _faker.Phone.PhoneNumber();
+
+    public string SsnHyphenated() => _faker.Random.ReplaceNumbers("###-##-####");
+
+    public string SsnNumeric() => _faker.Random.ReplaceNumbers("#########");
+
+    public string NonPiiString() => _faker.Lorem.Sentence();
+}
